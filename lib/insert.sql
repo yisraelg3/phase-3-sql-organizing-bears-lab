3968c49ca43c881e23585657ec8e4a6add9efd5f
Yisrael Gurkow <yisrelg3@gmail.com>
2021-06-11 15:42:41 -0400

    commit tree 62e83b1e5034e8b00cf3005aa1e86a048cdb6309

---
+++ lib/insert.sql
@@ -0,0 +1,9 @@
+INSERT INTO bears (name, age, sex, color, temperament, alive)
+    VALUES('Mr. Chocolate', 10, 'M', 'Brown', 'patient', 0),
+    ('Rowdy', 2, 'M', 'White', 'angry', 1),
+    ('Tabitha', 5, 'F', 'Black', 'patient', 0),
+    ('Sergeant Brown', 10, 'M', 'Brown', 'patient', 1),
+    ('Melissa', 5, 'M', 'Brown', 'angry', 1),
+    ('Grinch', 2, 'F', 'Black', 'angry', 1),
+    ('Wendy', 5, 'M', 'Brown', 'snappy', 1),
+    (NULL, 15, 'F', 'Black', 'patient', 0);
